19f34dca35ceb9a87d01542a009fd31cf81b4369
Liviu Ionescu <ilg@livius.net>
2021-02-12 21:11:50 +0200

    add support for CMake find_package()

--- meta/xpack-helper.cmake
+++ meta/xpack-helper.cmake
@@ -9,25 +9,31 @@
 #
 # -----------------------------------------------------------------------------
 
+if(micro-os-plus-libs-c-included)
+  return()
+endif()
+
+set(micro-os-plus-libs-c-included TRUE)
+
 message(STATUS "Including micro-os-plus-libs-c...")
 
 # -----------------------------------------------------------------------------
 
 function(target_sources_micro_os_plus_libs_c target)
 
-  get_filename_component(xpack_root_folder ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
+  get_filename_component(xpack_current_folder ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
 
   target_sources(
     ${target}
 
-    PRIVATE
-      ${xpack_root_folder}/src/stdlib/init-fini.c
-      ${xpack_root_folder}/src/stdlib/assert.c
-      ${xpack_root_folder}/src/stdlib/atexit.cpp
-      ${xpack_root_folder}/src/stdlib/timegm.c
-      ${xpack_root_folder}/src/stdlib/exit.c
-      ${xpack_root_folder}/src/_sbrk.c
-      ${xpack_root_folder}/src/c-syscalls-empty.cpp
+    PUBLIC
+      ${xpack_current_folder}/src/stdlib/init-fini.c
+      ${xpack_current_folder}/src/stdlib/assert.c
+      ${xpack_current_folder}/src/stdlib/atexit.cpp
+      ${xpack_current_folder}/src/stdlib/timegm.c
+      ${xpack_current_folder}/src/stdlib/exit.c
+      ${xpack_current_folder}/src/_sbrk.c
+      ${xpack_current_folder}/src/c-syscalls-empty.cpp
   )
 
 endfunction()
@@ -36,14 +42,84 @@ endfunction()
 
 function(target_include_directories_micro_os_plus_libs_c target)
 
-  get_filename_component(xpack_root_folder ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
+  get_filename_component(xpack_current_folder ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
 
   target_include_directories(
     ${target}
 
     PUBLIC
-      ${xpack_root_folder}/include
+      ${xpack_current_folder}/include
   )
+
+endfunction()
+
+# -----------------------------------------------------------------------------
+
+function(target_compile_definitions_micro_os_plus_libs_c target)
+
+  # None
+
+endfunction()
+
+# =============================================================================
+
+function(add_libraries_micro_os_plus_libs_c)
+
+  # ---------------------------------------------------------------------------
+
+  find_package(micro-os-plus-diag-trace)
+  find_package(micro-os-plus-startup)
+  
+  # ---------------------------------------------------------------------------
+
+  if(NOT TARGET micro-os-plus-libs-c-objects)
+
+    add_library(micro-os-plus-libs-c-objects OBJECT EXCLUDE_FROM_ALL)
+
+    target_sources_micro_os_plus_libs_c(micro-os-plus-libs-c-objects)
+    target_include_directories_micro_os_plus_libs_c(micro-os-plus-libs-c-objects)
+    target_compile_definitions_micro_os_plus_libs_c(micro-os-plus-libs-c-objects)
+
+    add_library(micro-os-plus::libs-c ALIAS micro-os-plus-libs-c-objects)
+    message(STATUS "micro-os-plus::libs-c")
+
+    target_link_libraries(
+      micro-os-plus-libs-c-objects
+
+      PUBLIC
+        micro-os-plus::common
+        micro-os-plus::diag-trace
+        micro-os-plus::startup
+        micro-os-plus::architecture
+    )
+
+  endif()
+
+  # ---------------------------------------------------------------------------
+
+  if(NOT TARGET micro-os-plus-libs-c-static)
+
+    add_library(micro-os-plus-libs-c-static STATIC EXCLUDE_FROM_ALL)
+
+    target_sources_micro_os_plus_libs_c(micro-os-plus-libs-c-static)
+    target_include_directories_micro_os_plus_libs_c(micro-os-plus-libs-c-static)
+    target_compile_definitions_micro_os_plus_libs_c(micro-os-plus-libs-c-static)
+
+    add_library(micro-os-plus::libs-c-static ALIAS micro-os-plus-libs-c-static)
+
+    target_link_libraries(
+      micro-os-plus-libs-c-static
+      
+      PUBLIC
+        micro-os-plus::common
+        micro-os-plus::diag-trace-static
+        micro-os-plus::startup-static
+        micro-os-plus::architecture
+    )
+
+  endif()
+
+  # ---------------------------------------------------------------------------
 
 endfunction()
 
