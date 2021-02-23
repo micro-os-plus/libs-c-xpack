db7167bb0a16ed05ec5ed60a319c3de6a7b9c766
Liviu Ionescu <ilg@livius.net>
2021-02-23 10:22:06 +0200

    cmake: simplify, remove functions

--- meta/xpack-helper.cmake
+++ meta/xpack-helper.cmake
@@ -19,51 +19,9 @@ message(STATUS "Including micro-os-plus-libs-c...")
 
 # -----------------------------------------------------------------------------
 
-function(target_sources_micro_os_plus_libs_c target)
-
-  get_filename_component(xpack_current_folder ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
-
-  target_sources(
-    ${target}
-
-    PRIVATE
-      ${xpack_current_folder}/src/stdlib/init-fini.c
-      ${xpack_current_folder}/src/stdlib/assert.c
-      ${xpack_current_folder}/src/stdlib/atexit.cpp
-      ${xpack_current_folder}/src/stdlib/timegm.c
-      ${xpack_current_folder}/src/stdlib/exit.c
-      ${xpack_current_folder}/src/_sbrk.c
-      ${xpack_current_folder}/src/c-syscalls-empty.cpp
-  )
-
-endfunction()
-
-# -----------------------------------------------------------------------------
-
-function(target_include_directories_micro_os_plus_libs_c target)
-
-  get_filename_component(xpack_current_folder ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
-
-  target_include_directories(
-    ${target}
-
-    PUBLIC
-      ${xpack_current_folder}/include
-  )
-
-endfunction()
-
-# -----------------------------------------------------------------------------
-
-function(target_compile_definitions_micro_os_plus_libs_c target)
-
-  # None
-
-endfunction()
-
-# =============================================================================
-
 function(add_libraries_micro_os_plus_libs_c)
+
+  get_filename_component(xpack_current_folder ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
 
   # ---------------------------------------------------------------------------
 
@@ -72,25 +30,49 @@ function(add_libraries_micro_os_plus_libs_c)
   
   # ---------------------------------------------------------------------------
 
-  if(NOT TARGET micro-os-plus-libs-c-static)
+  if(NOT TARGET micro-os-plus-libs-c-interface)
 
-    add_library(micro-os-plus-libs-c-static STATIC EXCLUDE_FROM_ALL)
+    # Since this library provides non-weak symbols to customise
+    # the newlib behaviour, it cannot be static, and the --whole-archive
+    # trick has other disadvantages.
+    add_library(micro-os-plus-libs-c-interface INTERFACE EXCLUDE_FROM_ALL)
 
-    target_sources_micro_os_plus_libs_c(micro-os-plus-libs-c-static)
-    target_include_directories_micro_os_plus_libs_c(micro-os-plus-libs-c-static)
-    target_compile_definitions_micro_os_plus_libs_c(micro-os-plus-libs-c-static)
+    # -------------------------------------------------------------------------
 
-    add_library(micro-os-plus::libs-c-static ALIAS micro-os-plus-libs-c-static)
-    message(STATUS "micro-os-plus::libs-c-static")
+    target_sources(
+      micro-os-plus-libs-c-interface
+  
+      INTERFACE
+        ${xpack_current_folder}/src/stdlib/init-fini.c
+        ${xpack_current_folder}/src/stdlib/assert.c
+        ${xpack_current_folder}/src/stdlib/atexit.cpp
+        ${xpack_current_folder}/src/stdlib/timegm.c
+        ${xpack_current_folder}/src/stdlib/exit.c
+        ${xpack_current_folder}/src/_sbrk.c
+        ${xpack_current_folder}/src/c-syscalls-empty.cpp
+    )
+
+    target_include_directories(
+      micro-os-plus-libs-c-interface
+  
+      INTERFACE
+        ${xpack_current_folder}/include
+    )
 
     target_link_libraries(
-      micro-os-plus-libs-c-static
+      micro-os-plus-libs-c-interface
       
-      PUBLIC
+      INTERFACE
         micro-os-plus::diag-trace-static
         micro-os-plus::startup-static
         micro-os-plus::architecture
     )
+
+    # -------------------------------------------------------------------------
+    # Aliases
+
+    add_library(micro-os-plus::libs-c ALIAS micro-os-plus-libs-c-interface)
+    message(STATUS "micro-os-plus::libs-c")
 
   endif()
 
