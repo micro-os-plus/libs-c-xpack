b99facb311f4f9297baa2e251ae18782bc53ae45
Liviu Ionescu <ilg@livius.net>
2021-02-25 15:43:01 +0200

    cmake: simplify, remove functions

--- meta/xpack-helper.cmake
+++ meta/xpack-helper.cmake
@@ -18,66 +18,62 @@ set(micro-os-plus-libs-c-included TRUE)
 message(STATUS "Including micro-os-plus-libs-c...")
 
 # -----------------------------------------------------------------------------
+# Dependencies.
 
-function(add_libraries_micro_os_plus_libs_c)
+find_package(micro-os-plus-diag-trace REQUIRED)
+find_package(micro-os-plus-startup REQUIRED)
 
-  get_filename_component(xpack_current_folder ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
+# -----------------------------------------------------------------------------
+# The current folder.
 
-  # ---------------------------------------------------------------------------
+get_filename_component(xpack_current_folder ${CMAKE_CURRENT_LIST_DIR} DIRECTORY)
 
-  find_package(micro-os-plus-diag-trace)
-  find_package(micro-os-plus-startup)
-  
-  # ---------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
-  if(NOT TARGET micro-os-plus-libs-c-interface)
+if(NOT TARGET micro-os-plus-libs-c-interface)
 
-    # Since this library provides non-weak symbols to customise
-    # the newlib behaviour, it cannot be static, and the --whole-archive
-    # trick has other disadvantages.
-    add_library(micro-os-plus-libs-c-interface INTERFACE EXCLUDE_FROM_ALL)
-
-    # -------------------------------------------------------------------------
-
-    target_sources(
-      micro-os-plus-libs-c-interface
-  
-      INTERFACE
-        ${xpack_current_folder}/src/stdlib/init-fini.c
-        ${xpack_current_folder}/src/stdlib/assert.c
-        ${xpack_current_folder}/src/stdlib/atexit.cpp
-        ${xpack_current_folder}/src/stdlib/timegm.c
-        ${xpack_current_folder}/src/stdlib/exit.c
-        ${xpack_current_folder}/src/_sbrk.c
-        ${xpack_current_folder}/src/c-syscalls-empty.cpp
-    )
-
-    target_include_directories(
-      micro-os-plus-libs-c-interface
-  
-      INTERFACE
-        ${xpack_current_folder}/include
-    )
-
-    target_link_libraries(
-      micro-os-plus-libs-c-interface
-      
-      INTERFACE
-        micro-os-plus::diag-trace-static
-        micro-os-plus::startup-static
-        micro-os-plus::architecture
-    )
-
-    # -------------------------------------------------------------------------
-    # Aliases
-
-    add_library(micro-os-plus::libs-c ALIAS micro-os-plus-libs-c-interface)
-    message(STATUS "micro-os-plus::libs-c")
-
-  endif()
+  # Since this library provides non-weak symbols to customise
+  # the newlib behaviour, it cannot be static, and the --whole-archive
+  # trick has other disadvantages.
+  add_library(micro-os-plus-libs-c-interface INTERFACE EXCLUDE_FROM_ALL)
 
   # ---------------------------------------------------------------------------
 
-endfunction()
+  target_sources(
+    micro-os-plus-libs-c-interface
+
+    INTERFACE
+      ${xpack_current_folder}/src/stdlib/init-fini.c
+      ${xpack_current_folder}/src/stdlib/assert.c
+      ${xpack_current_folder}/src/stdlib/atexit.cpp
+      ${xpack_current_folder}/src/stdlib/timegm.c
+      ${xpack_current_folder}/src/stdlib/exit.c
+      ${xpack_current_folder}/src/_sbrk.c
+      ${xpack_current_folder}/src/c-syscalls-empty.cpp
+  )
+
+  target_include_directories(
+    micro-os-plus-libs-c-interface
+
+    INTERFACE
+      ${xpack_current_folder}/include
+  )
+
+  target_link_libraries(
+    micro-os-plus-libs-c-interface
+    
+    INTERFACE
+      micro-os-plus::diag-trace-static
+      micro-os-plus::startup-static
+      micro-os-plus::architecture
+  )
+
+  # ---------------------------------------------------------------------------
+  # Aliases.
+
+  add_library(micro-os-plus::libs-c ALIAS micro-os-plus-libs-c-interface)
+  message(STATUS "micro-os-plus::libs-c")
+
+endif()
 
 # -----------------------------------------------------------------------------
