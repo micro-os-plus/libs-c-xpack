5a403d577fcd3ddcfaf360ba773b8a3d9dc830c2
Liviu Ionescu <ilg@livius.net>
2021-03-04 00:32:50 +0200

    cmake: add xpack_process_package_version()

--- meta/micro-os-plus-libs-c-config.cmake
+++ meta/micro-os-plus-libs-c-config.cmake
@@ -18,7 +18,7 @@ endif()
 
 set(micro-os-plus-libs-c-included TRUE)
 
-message(STATUS "Including micro-os-plus-libs-c...")
+message(STATUS "Processing xPack ${PACKAGE_JSON_NAME}@${PACKAGE_JSON_VERSION}...")
 
 # -----------------------------------------------------------------------------
 # Dependencies.
